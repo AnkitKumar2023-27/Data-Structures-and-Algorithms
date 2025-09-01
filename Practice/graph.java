a941d14a3373ec6fea19e81f6c1fd79a5223ed66
Ankit Kumar <ankitk07306@gmail.com>
2025-09-01 13:44:14 +0530

    01/09/25

--- Practice/graph.java
+++ Practice/graph.java
@@ -144,8 +144,159 @@ public static boolean IsCycle_Util(ArrayList<Eadge>[] graph, int curr, boolean v
     return false;
 }
 
-  
+//bipertype  graph//\
 
+
+public static boolean isBypertype(ArrayList<Eadge>graph[]){
+   int col[]=new int[ graph.length];
+   Arrays.fill(col,-1);
+     Queue<Integer>q=new LinkedList<>();
+     for( int i=0;i<graph.length;i++){
+           if(col[i]==-1){
+             col[i]=0;
+             q.add(i);
+           
+           }     };
+
+    
+              while (!q.isEmpty()) {
+                int curr=q.remove();
+                for( int i=0; i<graph[curr].size();i++){
+                  Eadge e=graph[curr].get(i);
+                if(col[e.dest]!=-1){
+                      int NextCol=col[curr]==0?1:0;
+                      col[e.dest]=NextCol;
+                      q.add(e.dest);
+                }else if(col[curr]==col[e.dest]){
+                  return false;
+                }
+                }
+               
+            }
+            return true;
+
+          
+       
+};
+
+ public static void graphCreation2(ArrayList<Eadge>graph[]){
+
+    for ( int  i=0;i<graph.length;i++){
+        graph[i] =new ArrayList<>();
+    };
+   
+      graph[2].add(new Eadge(2, 3, 1));
+      graph[3].add(new Eadge(3,1,1));
+      graph[4].add(new Eadge(4,0,1));
+      graph[4].add(new Eadge(4,4,1));
+      graph[5].add(new Eadge(5,0,1));
+    
+      graph[5].add(new Eadge(5,2,1));
+
+
+   }
+//topogycal Sort  using bfs
+public static void calInd( ArrayList<Eadge>graph[], int indegree[]){
+  for( int i=0;i<graph.length;i++){
+    for( int j=0 ; j<graph[i].size();j++){
+      Eadge e=graph[i].get(j);
+      indegree[e.dest]++;
+    }
+  }
+
+}
+public static void topogycalSort( ArrayList<Eadge>graph[]){
+   Queue<Integer>q=new LinkedList<>();
+   int indegree[]=new int[graph.length];
+    calInd(graph, indegree);
+
+    for(int i=0;i<graph.length;i++){
+       if(indegree[i]==0){
+        q.add(i);
+       }
+
+    }
+     while (!q.isEmpty()) {
+      int curr=q.remove();
+      System.out.print(curr+" ");
+      for( int i=0;i<graph[curr].size();i++){
+        Eadge e=graph[curr].get(i);
+         indegree[e.dest]--;
+        if(indegree[e.dest]==0){
+          q.add(e.dest);
+        }
+
+    
+      }
+      
+     }
+
+
+    }
+
+
+    // graph for the minimum spanning tree
+
+
+ public static void graphCreation3(ArrayList<Eadge>graph[]){
+
+    for ( int  i=0;i<graph.length;i++){
+        graph[i] =new ArrayList<>();
+    };
+   
+      graph[0].add(new Eadge(0, 1, 10));
+      graph[0].add(new Eadge(0,2,15));
+      graph[0].add(new Eadge(0,3,30));
+      graph[1].add(new Eadge(1,0,10));
+      graph[1].add(new Eadge(1,3,40));
+    
+      graph[2].add(new Eadge(2,0,15));
+       graph[2].add(new Eadge(2,3,50));
+         graph[3].add(new Eadge(3,1,40));
+           graph[3].add(new Eadge(3,2,15));
+
+   }
+   
+    public static class pair2 implements Comparable<pair2> {
+        int v;
+         int cost;
+         public pair2(int v, int cost){
+             this.v=v;
+             this.cost=cost;
+             
+         }
+         @Override
+         public int compareTo(pair2 p2) {
+             return this.cost-p2.cost;
+          
+         }
+
+    
+      
+    }
+
+public static void prims(ArrayList<Eadge>graph[]){
+  boolean  vis[]=new boolean[graph.length];
+  PriorityQueue<pair2>pq=new PriorityQueue<>();
+  pq.add(new pair2(0, 0));
+  int MinTotalCost=0;
+  while(!pq.isEmpty()){
+    pair2 curr=pq.remove();
+
+    if(!vis[curr.v]){
+      vis[curr.v]=true;
+      MinTotalCost+=curr.cost;
+
+      for(int i=0;i<graph[curr.v].size();i++){
+        Eadge e=graph[curr.v].get(i);
+        pq.add(new pair2(e.dest,e.wt));
+      }
+
+
+    }
+  }
+    System.out.println(MinTotalCost);
+}
     public static void main(String[] args) {
         int v=7;
         @SuppressWarnings("unchecked")
@@ -166,6 +317,31 @@ public static boolean IsCycle_Util(ArrayList<Eadge>[] graph, int curr, boolean v
 
         System.out.println("cycle Detection");
         System.out.println(IsyCycle(graph));
+
+        System.out.println();
+        System.out.println("Cheking Of Bypertype");
+        System.out.println(isBypertype(graph));
+
+        //topologycal graph
+
+        System.out.println("topologycal sort by bFS");
+        int u=7;
+        @SuppressWarnings("unchecked")
+        ArrayList<Eadge>graph2[]=new ArrayList[u];
+        graphCreation2(graph2);
+        topogycalSort(graph2);
+
+      System.out.println();
+      System.out.println("Minmum spanning tree");
+
+        //minimum sapnning tree
+        int w=9; 
+    @SuppressWarnings("unchecked")
+    ArrayList<Eadge>    graph3[]=new ArrayList[w];
+      graphCreation3(graph3);
+      prims(graph3);
+        
+    
     }
     
 }
